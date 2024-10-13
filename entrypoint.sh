ae20dbe58e8d472b69a9674a8997878257f23c08
Vlad Bors <74885743+MurkyPuma@users.noreply.github.com>
2024-10-13 09:47:59 -0700

    Update file path to .ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -l
 
-echo "Host $1" >> /root/.ssh/config
+echo "Host $1" >> ~/.ssh/config
 
 if [ -z $7 ] && [ -z $8 ]
 then
-    echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
+    echo "ProxyCommand cloudflared access ssh --hostname %h" >> ~/.ssh/config
 else
-    echo "ProxyCommand cloudflared access ssh --hostname %h --id $7 --secret $8" >> /root/.ssh/config
+    echo "ProxyCommand cloudflared access ssh --hostname %h --id $7 --secret $8" >> ~/.ssh/config
 fi
 
-echo "$5" > /root/.ssh/$4
-chmod 600 /root/.ssh/$4
+echo "$5" > ~/.ssh/$4
+chmod 600 ~/.ssh/$4
 
-ssh-keyscan $1 >> /root/.ssh/known_hosts
+ssh-keyscan $1 >> ~/.ssh/known_hosts
 
 ssh -T -q -o StrictHostKeyChecking=no $3@$1
 
-ssh -i /root/.ssh/$4 $3@$1 -p $2 "$6"
+ssh -i ~/.ssh/$4 $3@$1 -p $2 "$6"
